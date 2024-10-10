4f490d9d823a4a164f6746bf8f43864eeb2ba165
Pro100sasha_HD <sasha_ablin@rambler.ru>
2024-10-11 01:47:54 +0500

    task1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,9 +6,26 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
+    Point(){
+    }
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    void flip(){
+        x *= -1;
+        y *= -1;
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    double distance(Point point){
+        int x_distanse = Math.max(x,point.x) - Math.min(x,point.x);
+        int y_distanse = Math.max(y,point.y) - Math.min(y,point.y);
+        return Math.sqrt(Math.pow(x_distanse,2) + Math.pow(y_distanse,2));
+    }
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
